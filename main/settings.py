108334f38cfd0d8c3670eb5e9a26a34b48d50ccb
Ndeda <ndedaanaco@gmail.com>
2022-03-22 12:05:06 +0300

    fixed whitenoise error

--- main/settings.py
+++ main/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
@@ -68,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-'whitenoise.middleware.WhiteNoiseMiddleware',
+
             ],
         },
     },
